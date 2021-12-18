d40e2aed673dbdb6952eb79193f679da8dc065f4
KimTaeyeon-08 <ssg9732@gmail.com>
2021-12-19 02:24:58 +0900

    upload only scripts

---
+++ ButtonManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+public class ButtonManager : MonoBehaviour
+{
+    public GameObject sphere;
+    public VideoClip clip01;
+    public VideoClip clip02;
+    public GameObject button01;
+    public GameObject button02;
+    public GameObject button03;
+  public void toVideoScene()
+    {
+        SceneManager.LoadScene("360LondonPlay");
+    }
+    public void goToPark()
+    {
+        button01.SetActive(false);
+        button02.SetActive(true);
+        sphere.GetComponent<VideoPlayer>().clip = clip02;
+    }
+    public void goBacktoTower()
+    {
+        button01.SetActive(true);
+        button02.SetActive(false);
+        sphere.GetComponent<VideoPlayer>().clip = clip01;
+        Finish();
+    }
+    public void Finish()
+    {
+       button03.SetActive(true);
+       SceneManager.LoadScene("360Menu");
+    }
+}
---
+++ RayCast.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;//UI관련 사용하기 위해서
+public class RayCast : MonoBehaviour
+{
+    public Image reticle;//UI있어야함
+    float timeElapsed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        raycasting();
+    }
+    void raycasting()
+    {
+        RaycastHit hit;
+        Vector3 forward = transform.TransformDirection(Vector3.forward * 1000);
+        if(Physics.Raycast(transform.position,forward,out hit))
+        {
+            //버튼 게이지 채욱리
+            //지금까지 지내온 시간
+            timeElapsed = Time.deltaTime + timeElapsed;//한프레임당 걸린 시간(변동가능) + timeElapsed 
+            reticle.fillAmount = timeElapsed / 3;//reticle에 있는 fill amount 값을 3으로 나누기, 3초 세면 3 나눠서 1 될 수 있게, 3초 기다리기 
+            if (timeElapsed >= 3)
+            {
+                timeElapsed = 3;
+                hit.transform.GetComponent<Button>().onClick.Invoke();//raycast로 부딪힌 오브젝트의 온 클릭 기능 실행해라
+            }
+            
+            Debug.Log("hit");
+        }
+        //여기까지만 하면 button 밖에 있으면 게이지가 멈춤
+        //버튼 밖에서 게이지 줄이기
+        else
+        {
+            timeElapsed = timeElapsed - Time.deltaTime;
+            reticle.fillAmount = timeElapsed / 3;
+            if (timeElapsed <= 0) timeElapsed = 0;
+        }
+        Debug.DrawRay(transform.position, forward, Color.red);
+    }
+}
